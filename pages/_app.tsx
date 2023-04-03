05f493907571cde10a361b6e8caa493a40a31649
Hailey0930 <hyerin0930@gmail.com>
2023-04-03 18:31:48 +0900

    Add Naver Login & Edit Google Login (Edit env file setting)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,6 @@
 import { Global } from "@emotion/react";
 import { AppProps } from "next/app";
+import Head from "next/head";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { RecoilRoot } from "recoil";
 import { globalStyles } from "../src/commons/styles/globalStyles";
@@ -8,12 +9,21 @@ function MyApp({ Component, pageProps }: AppProps) {
   const queryClient = new QueryClient();
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <RecoilRoot>
-        <Global styles={globalStyles} />
-        <Component {...pageProps} />
-      </RecoilRoot>
-    </QueryClientProvider>
+    <>
+      <Head>
+        <script
+          src="https://static.nid.naver.com/js/naveridlogin_js_sdk_2.0.2.js"
+          defer
+          type="text/javascript"
+        ></script>
+      </Head>
+      <QueryClientProvider client={queryClient}>
+        <RecoilRoot>
+          <Global styles={globalStyles} />
+          <Component {...pageProps} />
+        </RecoilRoot>
+      </QueryClientProvider>
+    </>
   );
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,26 @@
 import axios from "axios";
+import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { useAuthToken } from "../src/commons/api/hooks/login/useAuthToken";
+
+declare global {
+  interface Window {
+    naver: any;
+  }
+}
 
 export default function Home() {
   const [, setAccessToken] = useState({ email: "", name: "" });
 
   const { mutate } = useAuthToken();
 
+  // NOTE Google
   useEffect(() => {
     const url = new URL(window.location.href);
-    const hash = url.hash;
+    const googleHash = url.hash;
 
-    if (hash) {
-      const splitToken = hash.split("=")[1].split("&")[0];
+    if (googleHash) {
+      const splitToken = googleHash.split("=")[1].split("&")[0];
 
       axios
         .get(
@@ -20,28 +28,55 @@ export default function Home() {
             splitToken,
           {
             headers: {
-              authorization: `${splitToken}`,
+              authorization: splitToken,
               accept: "application/json",
             },
           }
         )
         .then((data: any) => {
           setAccessToken({ email: data.data.email, name: data.data.id });
-          mutate({ type: "GOOGLE", token: data.config.headers.authorization });
+          mutate({ type: "GOOGLE", token: splitToken });
           console.log(data);
         })
         .catch((err) => console.log(err));
     }
   }, []);
 
-  const GOOGLE_CLIENT_ID: string = process.env
-    .NEXT_PUBLIC_GOOGLE_CLIENT_ID as string;
+  // NOTE Naver
+  useEffect(() => {
+    const naverLogin = new window.naver.LoginWithNaverId({
+      clientId: process.env.NEXT_PUBLIC_NAVER_CLIENT_ID,
+      callbackUrl: "http://localhost:3000",
+      loginButton: {
+        color: "green",
+        type: 2,
+        height: "30",
+      },
+    });
+    naverLogin.init();
 
-  const oAuthURL = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${GOOGLE_CLIENT_ID}&redirect_uri=http://localhost:3000&response_type=token&scope=https://www.googleapis.com/auth/userinfo.email`;
+    try {
+      naverLogin.getLoginStatus((data: any) => {
+        if (data) {
+          console.log("token", naverLogin.accessToken.accessToken);
+          setAccessToken({ email: data.email, name: data.name });
+        }
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
 
   const onClickGoogle = () => {
+    const oAuthURL = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}&redirect_uri=http://localhost:3000&response_type=token&scope=https://www.googleapis.com/auth/userinfo.email`;
+
     window.location.assign(oAuthURL);
   };
 
-  return <button onClick={onClickGoogle}>구글 로그인</button>;
+  return (
+    <>
+      <button onClick={onClickGoogle}>구글 로그인</button>
+      <button id="naverIdLogin" />
+    </>
+  );
 }
