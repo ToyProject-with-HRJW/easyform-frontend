b53487851dd6169e64b5d17d686a5c17d2c8952e
Hailey0930 <hyerin0930@gmail.com>
2023-04-04 12:19:27 +0900

    Edit next.config.js for CORS Error (not solved)

--- next.config.js
+++ next.config.js
@@ -6,6 +6,15 @@ const nextConfig = {
   env: {
     BASE_URL: process.env.BASE_URL,
   },
+
+  async rewrites() {
+    return [
+      {
+        source: "/:path*",
+        destination: "http://localhost:4000/:path*",
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
