7d64fe0784c8809fee2639b2954ae96e7c73a47e
Hailey0930 <hyerin0930@gmail.com>
2023-04-12 21:19:25 +0900

    Add Main Page UI #16

---
+++ pages/main/index.tsx
@@ -0,0 +1,5 @@
+import Main from "components/units/main/Main";
+
+export default function MainPage() {
+  return <Main></Main>;
+}
---
+++ src/components/units/main/Main.tsx
@@ -0,0 +1,135 @@
+import * as S from "./Main.styles";
+
+export default function Main() {
+  return (
+    <S.Wrapper>
+      <S.Title>Recent Forms</S.Title>
+      <S.ListWrapper>
+        <S.FormContainer>
+          <S.FormImageWrapper>
+            <S.FormImage />
+          </S.FormImageWrapper>
+          <S.FormInfoWrapper>
+            <S.FormInfoContainer>
+              <S.FormDisabledBadge>Not Accepting Responses</S.FormDisabledBadge>
+              <S.FormTitle>Account Approval Research</S.FormTitle>
+              <S.FormEditDate>edited 3days ago</S.FormEditDate>
+            </S.FormInfoContainer>
+            <S.ButtonWrapper>
+              <S.FormSeeMoreButton />
+            </S.ButtonWrapper>
+          </S.FormInfoWrapper>
+        </S.FormContainer>
+        <S.FormContainer>
+          <S.FormImageWrapper>
+            <S.FormImage />
+          </S.FormImageWrapper>
+          <S.FormInfoWrapper>
+            <S.FormInfoContainer>
+              <S.FormActiveBadge>Accepting Responses</S.FormActiveBadge>
+              <S.FormTitle>Account Approval Research</S.FormTitle>
+              <S.FormEditDate>edited 3days ago</S.FormEditDate>
+            </S.FormInfoContainer>
+            <S.ButtonWrapper>
+              <S.FormSeeMoreButton />
+            </S.ButtonWrapper>
+          </S.FormInfoWrapper>
+        </S.FormContainer>
+        <S.FormContainer>
+          <S.FormImageWrapper>
+            <S.FormImage />
+          </S.FormImageWrapper>
+          <S.FormInfoWrapper>
+            <S.FormInfoContainer>
+              <S.FormDisabledBadge>Not Accepting Responses</S.FormDisabledBadge>
+              <S.FormTitle>Account Approval Research</S.FormTitle>
+              <S.FormEditDate>edited 3days ago</S.FormEditDate>
+            </S.FormInfoContainer>
+            <S.ButtonWrapper>
+              <S.FormSeeMoreButton />
+            </S.ButtonWrapper>
+          </S.FormInfoWrapper>
+        </S.FormContainer>
+        <S.FormContainer>
+          <S.FormImageWrapper>
+            <S.FormImage />
+          </S.FormImageWrapper>
+          <S.FormInfoWrapper>
+            <S.FormInfoContainer>
+              <S.FormActiveBadge>Accepting Responses</S.FormActiveBadge>
+              <S.FormTitle>Account Approval Research</S.FormTitle>
+              <S.FormEditDate>edited 3days ago</S.FormEditDate>
+            </S.FormInfoContainer>
+            <S.ButtonWrapper>
+              <S.FormSeeMoreButton />
+            </S.ButtonWrapper>
+          </S.FormInfoWrapper>
+        </S.FormContainer>
+      </S.ListWrapper>
+
+      <S.Title>My Forms</S.Title>
+      <S.ListWrapper>
+        <S.FormContainer>
+          <S.FormImageWrapper>
+            <S.FormImage />
+          </S.FormImageWrapper>
+          <S.FormInfoWrapper>
+            <S.FormInfoContainer>
+              <S.FormDisabledBadge>Not Accepting Responses</S.FormDisabledBadge>
+              <S.FormTitle>Account Approval Research</S.FormTitle>
+              <S.FormEditDate>edited 3days ago</S.FormEditDate>
+            </S.FormInfoContainer>
+            <S.ButtonWrapper>
+              <S.FormSeeMoreButton />
+            </S.ButtonWrapper>
+          </S.FormInfoWrapper>
+        </S.FormContainer>
+        <S.FormContainer>
+          <S.FormImageWrapper>
+            <S.FormImage />
+          </S.FormImageWrapper>
+          <S.FormInfoWrapper>
+            <S.FormInfoContainer>
+              <S.FormActiveBadge>Accepting Responses</S.FormActiveBadge>
+              <S.FormTitle>Account Approval Research</S.FormTitle>
+              <S.FormEditDate>edited 3days ago</S.FormEditDate>
+            </S.FormInfoContainer>
+            <S.ButtonWrapper>
+              <S.FormSeeMoreButton />
+            </S.ButtonWrapper>
+          </S.FormInfoWrapper>
+        </S.FormContainer>
+        <S.FormContainer>
+          <S.FormImageWrapper>
+            <S.FormImage />
+          </S.FormImageWrapper>
+          <S.FormInfoWrapper>
+            <S.FormInfoContainer>
+              <S.FormDisabledBadge>Not Accepting Responses</S.FormDisabledBadge>
+              <S.FormTitle>Account Approval Research</S.FormTitle>
+              <S.FormEditDate>edited 3days ago</S.FormEditDate>
+            </S.FormInfoContainer>
+            <S.ButtonWrapper>
+              <S.FormSeeMoreButton />
+            </S.ButtonWrapper>
+          </S.FormInfoWrapper>
+        </S.FormContainer>
+        <S.FormContainer>
+          <S.FormImageWrapper>
+            <S.FormImage />
+          </S.FormImageWrapper>
+          <S.FormInfoWrapper>
+            <S.FormInfoContainer>
+              <S.FormActiveBadge>Accepting Responses</S.FormActiveBadge>
+              <S.FormTitle>Account Approval Research</S.FormTitle>
+              <S.FormEditDate>edited 3days ago</S.FormEditDate>
+            </S.FormInfoContainer>
+            <S.ButtonWrapper>
+              <S.FormSeeMoreButton />
+            </S.ButtonWrapper>
+          </S.FormInfoWrapper>
+        </S.FormContainer>
+      </S.ListWrapper>
+    </S.Wrapper>
+  );
+}
