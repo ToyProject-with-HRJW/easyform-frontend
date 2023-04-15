a11c6dbf58132ee8dbc34f54526bb44fd7e6a50f
Hailey0930 <hyerin0930@gmail.com>
2023-04-15 15:33:51 +0900

    Add main page modal (state 겹치는 문제 해결 필요 -> map 넣으면 key값을 끌고 와서 해결될지도?) #16

--- src/components/units/main/Main.styles.ts
+++ src/components/units/main/Main.styles.ts
@@ -199,13 +199,68 @@ export const FormEditDate = styled.div`
 
 export const ButtonWrapper = styled.div`
   margin: auto;
+  position: relative;
 `;
 
 export const FormSeeMoreButton = styled.div`
   width: 4px;
   height: 23px;
-  background-image: url("/assets/icon_seeMore.png");
+  background-image: url("/assets/main/icon_seeMore.png");
   background-repeat: no-repeat;
   background-position: center;
   cursor: pointer;
+`;
+
+interface IFormModal {
+  isDisplayRecent?: boolean;
+  isDisplayMy?: boolean;
+}
+
+export const FormModal = styled.div<IFormModal>`
+  display: ${(props) =>
+    props.isDisplayRecent ? "flex" : props.isDisplayMy ? "flex" : "none"};
+  flex-direction: column;
+  justify-content: space-between;
+  position: absolute;
+
+  top: 30px;
+  right: 0;
+  width: 322px;
+  height: 146px;
+  padding: 4px 0;
+  background-color: ${colors.black[950]};
+  border: 1px solid ${colors.black[700]};
+  box-shadow: 0px 0px 8px rgba(145, 145, 145, 0.2);
+  border-radius: 4px;
+`;
+
+export const FormModalWrapper = styled.div`
+  display: flex;
+  align-items: center;
+
+  width: 100%;
+  height: 40px;
+  padding: 0 20px;
+
+  :hover {
+    background-color: ${colors.blue[400]};
+  }
+`;
+
+export const ModalIcon = styled.div`
+  width: 16px;
+  height: 16px;
+  margin-right: 16px;
+`;
+
+export const ModalIconImage = styled.img`
+  width: 100%;
+  height: 100%;
+`;
+
+export const ModalText = styled.div`
+  font-weight: 400;
+  font-size: ${fontSize.deskTopFont.subTitle};
+  line-height: 17px;
+  color: ${colors.black[200]};
 `;
--- src/components/units/main/Main.tsx
+++ src/components/units/main/Main.tsx
@@ -1,6 +1,18 @@
+import { useState } from "react";
 import * as S from "./Main.styles";
 
 export default function Main() {
+  const [isDisplayRecent, setIsDisplayRecent] = useState(false);
+  const [isDisplayMy, setIsDisplayMy] = useState(false);
+
+  const onClickSeeMoreRecent = () => {
+    setIsDisplayRecent(!isDisplayRecent);
+  };
+
+  const onClickSeeMoreMy = () => {
+    setIsDisplayMy(!isDisplayMy);
+  };
+
   return (
     <S.Wrapper>
       <S.Title>Recent Forms</S.Title>
@@ -15,8 +27,28 @@ export default function Main() {
               <S.FormTitle>Account Approval Research</S.FormTitle>
               <S.FormEditDate>edited 3days ago</S.FormEditDate>
             </S.FormInfoContainer>
-            <S.ButtonWrapper>
+            <S.ButtonWrapper onClick={onClickSeeMoreRecent}>
               <S.FormSeeMoreButton />
+              <S.FormModal isDisplayRecent={isDisplayRecent}>
+                <S.FormModalWrapper>
+                  <S.ModalIcon>
+                    <S.ModalIconImage src="/assets/main/icon_share.png" />
+                  </S.ModalIcon>
+                  <S.ModalText>설문지 공유</S.ModalText>
+                </S.FormModalWrapper>
+                <S.FormModalWrapper>
+                  <S.ModalIcon>
+                    <S.ModalIconImage src="/assets/main/icon_delete.png" />
+                  </S.ModalIcon>
+                  <S.ModalText>삭제</S.ModalText>
+                </S.FormModalWrapper>
+                <S.FormModalWrapper>
+                  <S.ModalIcon>
+                    <S.ModalIconImage src="/assets/main/icon_copy.png" />
+                  </S.ModalIcon>
+                  <S.ModalText>복제</S.ModalText>
+                </S.FormModalWrapper>
+              </S.FormModal>
             </S.ButtonWrapper>
           </S.FormInfoWrapper>
         </S.FormContainer>
@@ -30,38 +62,28 @@ export default function Main() {
               <S.FormTitle>Account Approval Research</S.FormTitle>
               <S.FormEditDate>edited 3days ago</S.FormEditDate>
             </S.FormInfoContainer>
-            <S.ButtonWrapper>
+            <S.ButtonWrapper onClick={onClickSeeMoreRecent}>
               <S.FormSeeMoreButton />
-            </S.ButtonWrapper>
-          </S.FormInfoWrapper>
-        </S.FormContainer>
-        <S.FormContainer>
-          <S.FormImageWrapper>
-            <S.FormImage />
-          </S.FormImageWrapper>
-          <S.FormInfoWrapper>
-            <S.FormInfoContainer>
-              <S.FormDisabledBadge>Not Accepting Responses</S.FormDisabledBadge>
-              <S.FormTitle>Account Approval Research</S.FormTitle>
-              <S.FormEditDate>edited 3days ago</S.FormEditDate>
-            </S.FormInfoContainer>
-            <S.ButtonWrapper>
-              <S.FormSeeMoreButton />
-            </S.ButtonWrapper>
-          </S.FormInfoWrapper>
-        </S.FormContainer>
-        <S.FormContainer>
-          <S.FormImageWrapper>
-            <S.FormImage />
-          </S.FormImageWrapper>
-          <S.FormInfoWrapper>
-            <S.FormInfoContainer>
-              <S.FormActiveBadge>Accepting Responses</S.FormActiveBadge>
-              <S.FormTitle>Account Approval Research</S.FormTitle>
-              <S.FormEditDate>edited 3days ago</S.FormEditDate>
-            </S.FormInfoContainer>
-            <S.ButtonWrapper>
-              <S.FormSeeMoreButton />
+              <S.FormModal isDisplayRecent={isDisplayRecent}>
+                <S.FormModalWrapper>
+                  <S.ModalIcon>
+                    <S.ModalIconImage src="/assets/main/icon_share.png" />
+                  </S.ModalIcon>
+                  <S.ModalText>설문지 공유</S.ModalText>
+                </S.FormModalWrapper>
+                <S.FormModalWrapper>
+                  <S.ModalIcon>
+                    <S.ModalIconImage src="/assets/main/icon_delete.png" />
+                  </S.ModalIcon>
+                  <S.ModalText>삭제</S.ModalText>
+                </S.FormModalWrapper>
+                <S.FormModalWrapper>
+                  <S.ModalIcon>
+                    <S.ModalIconImage src="/assets/main/icon_copy.png" />
+                  </S.ModalIcon>
+                  <S.ModalText>복제</S.ModalText>
+                </S.FormModalWrapper>
+              </S.FormModal>
             </S.ButtonWrapper>
           </S.FormInfoWrapper>
         </S.FormContainer>
@@ -79,53 +101,28 @@ export default function Main() {
               <S.FormTitle>Account Approval Research</S.FormTitle>
               <S.FormEditDate>edited 3days ago</S.FormEditDate>
             </S.FormInfoContainer>
-            <S.ButtonWrapper>
+            <S.ButtonWrapper onClick={onClickSeeMoreMy}>
               <S.FormSeeMoreButton />
-            </S.ButtonWrapper>
-          </S.FormInfoWrapper>
-        </S.FormContainer>
-        <S.FormContainer>
-          <S.FormImageWrapper>
-            <S.FormImage />
-          </S.FormImageWrapper>
-          <S.FormInfoWrapper>
-            <S.FormInfoContainer>
-              <S.FormActiveBadge>Accepting Responses</S.FormActiveBadge>
-              <S.FormTitle>Account Approval Research</S.FormTitle>
-              <S.FormEditDate>edited 3days ago</S.FormEditDate>
-            </S.FormInfoContainer>
-            <S.ButtonWrapper>
-              <S.FormSeeMoreButton />
-            </S.ButtonWrapper>
-          </S.FormInfoWrapper>
-        </S.FormContainer>
-        <S.FormContainer>
-          <S.FormImageWrapper>
-            <S.FormImage />
-          </S.FormImageWrapper>
-          <S.FormInfoWrapper>
-            <S.FormInfoContainer>
-              <S.FormDisabledBadge>Not Accepting Responses</S.FormDisabledBadge>
-              <S.FormTitle>Account Approval Research</S.FormTitle>
-              <S.FormEditDate>edited 3days ago</S.FormEditDate>
-            </S.FormInfoContainer>
-            <S.ButtonWrapper>
-              <S.FormSeeMoreButton />
-            </S.ButtonWrapper>
-          </S.FormInfoWrapper>
-        </S.FormContainer>
-        <S.FormContainer>
-          <S.FormImageWrapper>
-            <S.FormImage />
-          </S.FormImageWrapper>
-          <S.FormInfoWrapper>
-            <S.FormInfoContainer>
-              <S.FormActiveBadge>Accepting Responses</S.FormActiveBadge>
-              <S.FormTitle>Account Approval Research</S.FormTitle>
-              <S.FormEditDate>edited 3days ago</S.FormEditDate>
-            </S.FormInfoContainer>
-            <S.ButtonWrapper>
-              <S.FormSeeMoreButton />
+              <S.FormModal isDisplayMy={isDisplayMy}>
+                <S.FormModalWrapper>
+                  <S.ModalIcon>
+                    <S.ModalIconImage src="/assets/main/icon_share.png" />
+                  </S.ModalIcon>
+                  <S.ModalText>설문지 공유</S.ModalText>
+                </S.FormModalWrapper>
+                <S.FormModalWrapper>
+                  <S.ModalIcon>
+                    <S.ModalIconImage src="/assets/main/icon_delete.png" />
+                  </S.ModalIcon>
+                  <S.ModalText>삭제</S.ModalText>
+                </S.FormModalWrapper>
+                <S.FormModalWrapper>
+                  <S.ModalIcon>
+                    <S.ModalIconImage src="/assets/main/icon_copy.png" />
+                  </S.ModalIcon>
+                  <S.ModalText>복제</S.ModalText>
+                </S.FormModalWrapper>
+              </S.FormModal>
             </S.ButtonWrapper>
           </S.FormInfoWrapper>
         </S.FormContainer>
