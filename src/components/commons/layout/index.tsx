f6379eb7e951d1a09455586c83bb57def51fb585
Hailey0930 <hyerin0930@gmail.com>
2023-04-28 19:38:28 +0900

    Setting create page & layout menu bar with create page

--- src/components/commons/layout/index.tsx
+++ src/components/commons/layout/index.tsx
@@ -1,21 +1,29 @@
 import { ReactNode } from "react";
 import Header from "./header/Header";
 import styled from "@emotion/styled";
+import MenuBar from "./menuBar/MenuBar";
 import SearchBar from "./searchBar/SearchBar";
 import MBWriteButton from "./MBWriteButton";
 import { setMobileStyle, setTabletStyle } from "commons/styles/mediaQuery";
 import { css } from "@emotion/react";
+import { useRouter } from "next/router";
 
 interface ILayoutProps {
   children: ReactNode;
 }
 
+const CREATE_PAGE = "/create";
+
 export default function Layout(props: ILayoutProps) {
+  const router = useRouter();
+
+  const createPage = router.asPath.includes(CREATE_PAGE);
+
   return (
     <>
       <Header />
       <SearchBarBodyWrapper>
-        <SearchBar />
+        {createPage ? <MenuBar></MenuBar> : <SearchBar />}
         {props.children}
       </SearchBarBodyWrapper>
       <MBWriteButton />
