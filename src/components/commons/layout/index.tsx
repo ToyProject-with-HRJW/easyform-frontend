5ebbf02aa0f7e36a59f7a22df885c5a9abd342c7
Hailey0930 <hyerin0930@gmail.com>
2023-05-31 19:49:37 +0900

    Add Login Page UI #36

--- src/components/commons/layout/index.tsx
+++ src/components/commons/layout/index.tsx
@@ -14,21 +14,29 @@ interface ILayoutProps {
 
 const CREATE_PAGE = "/create";
 const RESULT_PAGE = "/result";
+const LOGIN_PAGE = "/login";
 
 export default function Layout(props: ILayoutProps) {
   const router = useRouter();
 
+  const loginPage = router.asPath.includes(LOGIN_PAGE);
   const createPage = router.asPath.includes(CREATE_PAGE);
   const resultPage = router.asPath.includes(RESULT_PAGE);
 
   return (
     <>
-      <Header />
-      <SearchBarBodyWrapper>
-        {createPage || resultPage ? <MenuBar /> : <SearchBar />}
-        {props.children}
-      </SearchBarBodyWrapper>
-      <MBWriteButton />
+      {loginPage ? (
+        <SearchBarBodyWrapper>{props.children}</SearchBarBodyWrapper>
+      ) : (
+        <>
+          <Header />
+          <SearchBarBodyWrapper>
+            {createPage || resultPage ? <MenuBar /> : <SearchBar />}
+            {props.children}
+          </SearchBarBodyWrapper>
+          <MBWriteButton />
+        </>
+      )}
     </>
   );
 }
