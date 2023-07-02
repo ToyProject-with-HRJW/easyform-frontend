5b0c31037053f450bf3d3f46ed1162b4b4de8f16
Hailey0930 <hyerin0930@gmail.com>
2023-07-02 19:29:43 +0900

    add scroll to top & scroll to bottom #25

--- src/components/units/create/Create.tsx
+++ src/components/units/create/Create.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import * as S from "./Create.styles";
 import Question from "components/commons/question/Question";
 import { v4 as uuidv4 } from "uuid";
@@ -11,6 +11,28 @@ export default function Create() {
       id: uuidv4(),
     },
   ]);
+  const [scrollPosition, setScrollPosition] = useState(0);
+  const [scrollTopDown, setScrollTopDown] = useState("top");
+
+  const scrollRef = useRef<HTMLDivElement | null>(null);
+
+  // NOTE add question 버튼 클릭 시 스크롤 아래로 이동하도록 하는 useEffect
+  useEffect(() => {
+    scrollRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [addQuestion]);
+
+  // NOTE 현재 스크롤 위치 가져오는 useEffect
+  useEffect(() => {
+    if (window !== undefined) {
+      window.addEventListener("scroll", getScrollPosition);
+
+      if (scrollPosition < 300) {
+        setScrollTopDown("down");
+      } else if (scrollPosition >= 300) {
+        setScrollTopDown("top");
+      }
+    }
+  }, [scrollPosition, scrollTopDown]);
 
   const onClickEditTitle = () => {
     if (!isEditTitle) setIsEditTitle(true);
@@ -40,6 +62,32 @@ export default function Create() {
       setAddQuestion(
         addQuestion.filter((question: { id: string }) => question.id !== id)
       );
+  };
+
+  const onClickScrollToTop = () => {
+    if (window !== undefined) {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+    setScrollTopDown("down");
+  };
+
+  const onClickScrollToBottom = () => {
+    if (window !== undefined) {
+      window.scrollTo({
+        top: document.documentElement.scrollHeight,
+        behavior: "smooth",
+      });
+    }
+    setScrollTopDown("top");
+  };
+
+  const getScrollPosition = () => {
+    if (window !== undefined) {
+      setScrollPosition(window.scrollY || document.documentElement.scrollTop);
+    }
   };
 
   return (
@@ -76,11 +124,23 @@ export default function Create() {
           />
         ))}
       </S.QuestionWrapper>
-      <S.AddQuestionButtonWrapper>
+      <S.AddQuestionButtonWrapper ref={scrollRef}>
         <S.AddQuestionButton onClick={onClickAddQuestion}>
           Add Question
         </S.AddQuestionButton>
       </S.AddQuestionButtonWrapper>
+      {addQuestion.length > 2 && (
+        <S.ScrollButtonWrapper>
+          <S.ScrollButton
+            onClick={
+              scrollTopDown === "top"
+                ? onClickScrollToTop
+                : onClickScrollToBottom
+            }
+            scrollTopDown={scrollTopDown}
+          />
+        </S.ScrollButtonWrapper>
+      )}
     </S.Wrapper>
   );
 }
